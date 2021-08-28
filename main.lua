b1531f74cf96278bd37e48bf6c9a7bbe2a96c205
Joe Hinkle <joehinkle11@gmail.com>
2021-08-28 02:26:07 -0500

    game

--- main.lua
+++ main.lua
@@ -4,4 +4,11 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- include the Corona "composer" module
+local composer = require "composer"
+
+-- load menu screen
+composer.gotoScene( "menu" )
